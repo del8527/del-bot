0a76f8a67fa73c2e1b8852849eaa1e9fd4a2f6fc
Delbert Luo <delbert3luo@gmail.com>
2022-05-15 01:44:56 -0400

    fixed hypixel.js a bit

--- hypixel.js
+++ hypixel.js
@@ -1,5 +1,5 @@
-const fetch = require('node-fetch');
-let API_KEY = process.env.API_KEY;
+import fetch from 'node-fetch';
+let API_KEY = (process.env.API_KEY);
 
 const playerName = "lpmessi"
 const playerUUID = "7aaf4557-1b5d-4e07-bdca-4e7ff2429872"
