44ba9e9e7db949c0f2bc840fcd765daaca88bed2
Delbert Luo <delbert3luo@gmail.com>
2022-05-15 03:40:12 -0400

    no es6

--- hypixel.js
+++ hypixel.js
@@ -1,12 +1,18 @@
+require("dotenv").config()
 import fetch from 'node-fetch';
-let API_KEY = (process.env.API_KEY);
+
+let MY_API_KEY = process.env.API_KEY;
 
 const playerName = "lpmessi"
 const playerUUID = "7aaf4557-1b5d-4e07-bdca-4e7ff2429872"
 
-fetch (`https://api.hypixel.net/player?key=${API_KEY}&name=${playerName}`)
+fetch ('https://api.hypixel.net/resources/achievements')
+
+
+fetch (`https://api.hypixel.net/player?key=${MY_API_KEY}&name=${playerName}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        console.log(data.player.achievements.pit_prestiges)
+        // console.log(data)
     })
     .catch(error => console.log("Network Error", error))
