60b2542a4a8ab3bce3c71629bea4b08febd3734f
Delbert <55643100+del8527@users.noreply.github.com>
2022-06-08 23:58:13 -0400

    o hours

--- commands/schedule.js
+++ commands/schedule.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'schedule, beta',
     execute(message, args) {
         message.channel.send(`M237 Mobius - Tuesday @ 4pm \n
-        M237 WA - Friday @ 9am`)
+        M237 WA - Friday @ 9am\n
+        CS246 OH - https://student.cs.uwaterloo.ca/~cs246/S22/officehours.shtml`)
     }
 }
