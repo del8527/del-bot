866afaca12a302799184624dccbbbae353d296d3
Delbert <55643100+del8527@users.noreply.github.com>
2022-07-30 15:06:52 -0400

    sched

--- commands/schedule.js
+++ commands/schedule.js
@@ -2,8 +2,7 @@ module.exports = {
     name: 'schedule',
     description: 'schedule, beta',
     execute(message, args) {
-        message.channel.send(`M237 Mobius - Tuesday @ 4pm \n
-        M237 WA - Friday @ 9am\n
-        CS246 OH - https://student.cs.uwaterloo.ca/~cs246/S22/officehours.shtml`)
+        message.channel.send(`Coming soon in Winter!`
+        )
     }
 }
