a4368ee8dbf4eed1143a6d888d3a68cd70f8fff4
Delbert <55643100+del8527@users.noreply.github.com>
2022-08-05 07:14:38 -0400

    cap

--- commands/img.js
+++ commands/img.js
@@ -8,11 +8,11 @@ const google = new Scraper({
   
 module.exports = {
     name: 'img',
-    description: 'Scrapes google for the image passed',
+    description: 'Scrapes Google for the image passed',
     async execute(message, args) {
         const image_query = args.join(' ');
         if(!image_query) { 
-            return message.channel.send('enter an image name!');
+            return message.channel.send('Enter an image name!');
         }
         const image_results = await google.scrape(image_query, 50);
         console.log('results', image_results)
